bdcc02615853b0ff435315aa5ca143a41617ace6
oddconverse <jarrahbeattie@gmail.com>
2025-09-03 12:27:38 +1000

    implemented load+save methods

--- RecordSystem.java
+++ RecordSystem.java
@@ -1,15 +1,17 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class RecordSystem {
+    // matches to be stored in array
     private Match[] matches;
     private int matchCount = 0;
-
+    // add match to array
     public void addMatch(String round, String homeTeamName, int homeTeamGoals, int homeTeamBehinds, String awayTeamName, int awayTeamGoals, int awayTeamBehinds) {
         matches[matchCount] = new Match(round, homeTeamName, homeTeamGoals, homeTeamBehinds, awayTeamName, awayTeamGoals, awayTeamBehinds);
         matchCount++;
     }
-
+    // save matches to text file to 
     public void saveMatches() throws Exception {
         try {
             File matchFile = new File("data.txt");
@@ -17,23 +19,39 @@ public class RecordSystem {
             for (int i = 0; i < matchCount; i++) {
                 Match currentMatch = matches[i];
                 matchWriter.write(String.format("%s; %s; %d; %d; %s; %d; %d\n", currentMatch.getRound(), currentMatch.getHomeTeamName(), currentMatch.getHomeTeamGoals(), currentMatch.getHomeTeamBehinds(), currentMatch.getAwayTeamName(), currentMatch.getAwayTeamGoals(), currentMatch.getAwayTeamBehinds()));
-                
             }
             matchWriter.close();
         }
         catch (Exception e) {
-
+            System.out.println(e);
         }
     }
 
-    public void loadMatches() {
+    public void loadMatches() throws Exception {
+        File matchFile = new File("data.txt");
+        Scanner matchInfile = new Scanner(matchFile);
 
+        try {
+            while (matchInfile.hasNextLine()) {
+                String s = matchInfile.nextLine();
+                StringTokenizer stk = new StringTokenizer(s, ";");
+                String round = stk.nextToken().trim();
+                String homeTeamName = stk.nextToken().trim();
+                int homeTeamGoals = Integer.parseInt(stk.nextToken().trim());
+                int homeTeamBehinds = Integer.parseInt(stk.nextToken().trim());
+                String awayTeamName = stk.nextToken().trim();
+                int awayTeamGoals = Integer.parseInt(stk.nextToken().trim());
+                int awayTeamBehinds = Integer.parseInt(stk.nextToken().trim());
+                addMatch(round, homeTeamName, homeTeamGoals, homeTeamBehinds, awayTeamName, awayTeamGoals, awayTeamBehinds);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        matchInfile.close();
     }
 
     public String findGreatestMargins() {
         return null;
-    }
-    public static void main(String[] args) {
-        
     }
 }
